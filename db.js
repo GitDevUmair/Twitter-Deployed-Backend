31f3f087763bcd83905d135e75b9e044c2e4afc2
Umair <iumair350@gmail.com>
2023-09-04 16:08:19 +0500

    modified db.js

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose')
-const mongoURI = 'mongodb://localhost:27017/twitter'
+const mongoose = require("mongoose");
+const mongoURI =
+  "mongodb+srv://umair:twitter0@cluster0.w1bblxn.mongodb.net/?retryWrites=true&w=majority";
 async function connectToMongo() {
-    try {
-      await mongoose.connect(mongoURI);
-      console.log('Connected to MongoDB successfully!');
-    } catch (err) {
-      console.error('Error connecting to MongoDB:', err);
-    }
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB successfully!");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
   }
-  
+}
+
 module.exports = connectToMongo;
